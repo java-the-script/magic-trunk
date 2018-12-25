eeff3d34431da47d86ac36c70f02427383952a85
JoeyDeol <gurjothdeol@gmail.com>
2018-12-25 01:46:07 -0500

    Implement initial crude routing
    Application now routes from landing page to openTrunk page. Need to add in the additional pages in order to be complete.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-class App extends Component {
+class ClosedTrunk extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Magic Trunk</h1>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Link to="/openTrunk">Open The Trunk</Link>
         </header>
-        <BrowserRouter></BrowserRouter>
       </div>
+    )
+  }
+}
+
+class OpenTrunk extends Component {
+  render() {
+    return (
+      <div>
+        <h1>THIS IS THE OPEN TRUNK!</h1>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="main-container">
+          <Route path="/" exact component={ClosedTrunk}></Route>
+          <Route path="/openTrunk" exact component={OpenTrunk}></Route>
+        </div>
+      </Router>
     );
   }
 }
