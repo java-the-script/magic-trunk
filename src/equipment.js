4953f1ac420026fdf751b5f4c892ee142ab4e3b0
JoeyDeol <gurjothdeol@gmail.com>
2019-01-01 18:55:42 -0500

    Add back links to equipment and house areas

--- src/equipment.js
+++ src/equipment.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Equipment extends Component {
   render() {
     return (
       <div>
         <h1>Equipment Portion</h1>
+        <Link to="/openTrunk">Back</Link>
       </div>
     )
   }
--- src/house.js
+++ src/house.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class House extends Component {
   render() {
     return (
       <div>
         <h1>House Portion</h1>
+        <Link to="/openTrunk">Back</Link>
       </div>
     )
   }
